dfb56e4accf40b97b2d7a85385e2b5fe6cf32f35
Arisaladboard <salehpoura@spu.edu>
2022-11-19 16:07:46 -0800

    comments added
    done

--- index.js
+++ index.js
@@ -16,17 +16,17 @@ app.set('view engine', 'ejs')
  // post route to add a task 
  app.post('/addtask', async (req,res) => {
    var newTask = req.body.newtask //gets new task
-    console.log("newTask")
+    console.log("newTask")     
 
-    const listFile = fs.readFileSync('./listdata.json', 'utf-8')
+    const listFile = fs.readFileSync('./listdata.json', 'utf-8')  // reads existing file 
     const list = JSON.parse(listFile) 
-    list.push(newTask)
+    list.push(newTask) // adds new task to array 
    
-    fs.writeFile('./listdata.json', JSON.stringify(list), err =>{
+    fs.writeFile('./listdata.json', JSON.stringify(list), err =>{ 
         if (err) {
             console.log("error")
         }
-    })
+    }) // pushes array and writes it to file 
 
 
     
@@ -35,15 +35,12 @@ app.set('view engine', 'ejs')
  })
 
  app.post('/removetask' ,(req,res) => {
-    var completeTask = req.body.check
-    // any task checked is uploaded into completetask 
-    // if it is one, then it pushs to complete array and removes from task array
-    // does the same in a forloop condition for multiple
-    // then redirects back to home display 
+    var completeTask = req.body.check // gets task/tasks to remove 
+    
 
-    const listFile = fs.readFileSync('./listdata.json', 'utf-8')
+    const listFile = fs.readFileSync('./listdata.json', 'utf-8') // reads file 
     const list = JSON.parse(listFile) 
-
+// finds task/tasks in array to remove then removes them
     if (typeof completeTask === "string"){
         list.splice(list.indexOf(completeTask),1)
     } else if (typeof completeTask === "object"){
@@ -51,7 +48,7 @@ app.set('view engine', 'ejs')
             list.splice(list.indexOf(completeTask))
         }
     }
-
+//writes new array to the file 
     fs.writeFile('./listdata.json', JSON.stringify(list), err =>{
         if (err) {
             console.log("error")
@@ -80,7 +77,11 @@ app.get('/',  async (req, res) =>{
 app.listen(5600);
 
 
-// things to note for the future 
+// Lessons I learned
 // do all package installations from the jump
 // all ejs files ( aka pages in simpler terms will be put in views)
 // to install packages you need to stop running ?? 
+// easier to do less files and more in each to be honest, the more files made it more 
+// youtube is sometimes your best friend, documentation lowkey is easier to decioher sometimes
+// break it up into parts, routes, ejs templates, reading/writing, then combine it all 
+// little steps and jumps help make sure there's no errors on the way 
